228521f32bd9699e0c2d5648acd1fc9ba71f8716
arteys <calamatica@gmail.com>
2023-04-15 22:18:59 +0500

    Good enough

--- Conversion_tool.py
+++ Conversion_tool.py
@@ -1,21 +1,39 @@
 import pandas as pd
+import tkinter as tk
+import tkinter.filedialog as fd
+import os
 
-path = "D:\Минин\BNTM-349.xlsx"
 
-data_df = pd.read_excel(path,skiprows=23,header=None)
-data_df.drop(columns=[0,1,14], axis=1, inplace=True)
+def conversion(path, label_list):
+    data_df = pd.read_excel(path,skiprows=23,header=None)
+    data_df.drop(columns=[0,1,14], axis=1, inplace=True)
 
+    result_labels = {label_list[0]:[],label_list[1]:[],label_list[2]:[],'Control':[]}
+    results = pd.DataFrame(result_labels)
+
+    #Concat 3 column
+    i = 2
+    for label in label_list:
+        results[label] = pd.concat([data_df[i],data_df[i+1],data_df[i+2]],ignore_index=True)
+        i = i+3
+
+    file_name_ext = os.path.basename(path)
+    folder_path = os.path.dirname(path)
+    file_name,extention = file_name_ext.split(".") 
+
+    # Check if folder /Image exists, if no - create one
+    if os.path.isdir(folder_path + '/Converted'):
+        print('Image folder already exists')
+    else:
+        os.mkdir(folder_path + '/Converted')
+
+    save_path = folder_path + '/Converted/' + file_name + '.csv'
+    results.to_csv(save_path,index=False)
+
+root = tk.Tk()
+paths = fd.askopenfilenames(parent=root, title='Open files')
 
 label_list = ['10-4','10-5','10-6','Control']
 
-result_labels = {label_list[0]:[],label_list[1]:[],label_list[2]:[],'Control':[]}
-results = pd.DataFrame(result_labels)
-
-print(data_df)
-
-i = 2
-for label in label_list:
-    results[label] = pd.concat([data_df[i],data_df[i+1],data_df[i+2]],ignore_index=True)
-    i = i+3
-
-print(results)
+for path in paths:
+    conversion(path,label_list)
