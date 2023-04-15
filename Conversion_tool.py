66029abcccaa343fe68ba500e56be01d368bf2cb
arteys <calamatica@gmail.com>
2023-04-15 16:14:12 +0500

    Working prototype

--- Conversion_tool.py
+++ Conversion_tool.py
@@ -2,6 +2,20 @@ import pandas as pd
 
 path = "D:\Минин\BNTM-349.xlsx"
 
-data_pd = pd.read_excel()
+data_df = pd.read_excel(path,skiprows=23,header=None)
+data_df.drop(columns=[0,1,14], axis=1, inplace=True)
 
-print(data_pd)
+
+label_list = ['10-4','10-5','10-6','Control']
+
+result_labels = {label_list[0]:[],label_list[1]:[],label_list[2]:[],'Control':[]}
+results = pd.DataFrame(result_labels)
+
+print(data_df)
+
+i = 2
+for label in label_list:
+    results[label] = pd.concat([data_df[i],data_df[i+1],data_df[i+2]],ignore_index=True)
+    i = i+3
+
+print(results)
