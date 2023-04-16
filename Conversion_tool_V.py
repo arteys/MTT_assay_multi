7bc7648184b8cde01c8f392bf7dd760118170a49
arteys <calamatica@gmail.com>
2023-04-16 23:04:36 +0500

    One ugly afterthought

--- Conversion_tool_V.py
+++ Conversion_tool_V.py
@@ -9,19 +9,24 @@ def conversion(path, label_list):
 
     data_df.drop(labels=[0,1,14], axis=1, inplace=True)
     data_df.drop(labels=[6], axis=0, inplace=True)
-
+    print(data_df)
     labels = data_df.columns
-    print(labels)
 
     result_labels = {label_list[0]:[],label_list[1]:[],label_list[2]:[],'Control':[]}
     results = pd.DataFrame(result_labels)
 
 
     #Concat 3 column
-    i = 2
-    for label in label_list:
-        results[label] = pd.concat([data_df[i],data_df[i+1],data_df[i+2]],ignore_index=True)
-        i = i+3
+    # i = 2
+    # for label in label_list:
+    #     results[label] = pd.concat([data_df[i],data_df[i+1],data_df[i+2]],ignore_index=True)
+    #     i = i+3
+    
+    results[label_list[0]] =  pd.concat([data_df[3],data_df[4]],ignore_index=True)
+    results[label_list[1]] =  pd.concat([data_df[5],data_df[6],data_df[7]],ignore_index=True)
+    results[label_list[2]] =  pd.concat([data_df[8],data_df[9],data_df[10]],ignore_index=True)
+    results[label_list[3]] =  pd.concat([data_df[11],data_df[12]],ignore_index=True)
+
 
     file_name_ext = os.path.basename(path)
     folder_path = os.path.dirname(path)
@@ -34,12 +39,11 @@ def conversion(path, label_list):
         os.mkdir(folder_path + '/Converted')
 
     save_path = folder_path + '/Converted/' + file_name + '.xlsx'
-    # results.to_excel(save_path,index=False)
+    results.to_excel(save_path,index=False)
 
-# root = tk.Tk()
-# paths = fd.askopenfilenames(parent=root, title='Open files')\
+root = tk.Tk()
+paths = fd.askopenfilenames(parent=root, title='Open files')
 
-paths = ["D:\\Минин\\07-04-2023 BNAB-110.xlsx"]
 
 label_list = ['10-4','10-5','10-6','Control']
 
